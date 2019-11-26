f0e1db269a5d14c5bfa571b1156a761ebe2c3298
RickardPetterson <rickardnp@gmail.com>
2019-11-26 21:40:08 +0100

    a

--- www/js/index.js
+++ www/js/index.js
@@ -3,29 +3,24 @@ var app = {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
     onDeviceReady: function() {
-		alert('OK1');
         var that = this;
         document.getElementById("start-scan").onclick = function() {
-			alert('OK2');
             cordova.plugins.barcodeScanner.scan(
 			  function (result) {
-				  alert("We got a barcode\n" +
-						"Result: " + result.text + "\n" +
-						"Format: " + result.format + "\n" +
-						"Cancelled: " + result.cancelled);
+				  that.updateResults(result);
 			  },
 			  function (error) {
-				  alert("Scanning failed: " + error);
+				  that.updateResults(error, true);
 			  },
 			  {
-				  preferFrontCamera : true, // iOS and Android
-				  showFlipCameraButton : true, // iOS and Android
-				  showTorchButton : true, // iOS and Android
-				  torchOn: true, // Android, launch with the torch switched on (if available)
-				  saveHistory: true, // Android, save scan history (default false)
+				  preferFrontCamera : false, // iOS and Android
+				  showFlipCameraButton : false, // iOS and Android
+				  showTorchButton : false, // iOS and Android
+				  torchOn: false, // Android, launch with the torch switched on (if available)
+				  saveHistory: false, // Android, save scan history (default false)
 				  prompt : "Place a barcode inside the scan area", // Android
 				  resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
-				  formats : "QR_CODE,PDF_417,CODE_128", // default: all but PDF_417 and RSS_EXPANDED
+				  //formats : "QR_CODE,PDF_417,CODE_128", // default: all but PDF_417 and RSS_EXPANDED
 				  orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
 				  disableAnimations : true, // iOS
 				  disableSuccessBeep: false // iOS and Android
